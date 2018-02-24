8048d15c056876bc792a29af4e94f49f8d2d74bd
Aluno <aluno@imac-de-suporte-8.local>
2018-02-24 09:40:50 -0300

    first commit

---
+++ imc/PresenterIMC.swift
@@ -0,0 +1,15 @@
+//
+//  PresenterIMC.swift
+//  imc
+//
+//  Created by Aluno on 24/02/2018.
+//  Copyright © 2018 [redcode]. All rights reserved.
+//
+
+import Foundation
+
+class PresenterIMC {
+    
+    
+    
+}
--- imc/ViewController.swift
+++ imc/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldWeight: UITextField!
+    @IBOutlet weak var textFieldHeight: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculate(_ sender: UIButton) {
+        
+        let stringWeight = getValuesFromField(field : textFieldWeight)
+        let stringHeight = getValuesFromField(field : textFieldHeight)
+        
+        if(stringWeight.isEmpty == false && stringHeight.isEmpty == false) {
+            
+            let weight = Float(stringWeight)!
+            let height = Float(stringHeight)!
+            
+            let result = weight / (height * weight)
+            print(String.init(format: "Seu peso é %f", result))
+        }
+    }
+    
+    private func getValuesFromField(field: UITextField!) -> String {
+        if let value = field?.text {
+            if (value.isEmpty == false) {
+                return value
+            }
+        }
+        
+        guard let placeholder = field.placeholder else {return ""}
+        print("Preencha o campo \(String(describing: placeholder ))")
+        
+        return ""
+    }
+    
 }
-
